6b2028c7803cc01e976f5b0f568629b6bbd3c737
pndparade <lemurof@yandex.ru>
2024-10-04 12:02:54 +0300

    delete unused files & add routes

---
+++ src/views/AuthView.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>Auth page</h1>
+</template>
+
+<style scoped></style>
---
+++ src/views/PostView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+    </nav>
+  </header>
+  <main>
+    <h1>Post page</h1>
+  </main>
+</template>
